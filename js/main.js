06b0b22eedc344551186a790e21da4fe2c8f0f99
jfemon8 <jfemon8@gmail.com>
2025-02-28 21:09:07 +0600

    Completed all sections

--- js/main.js
+++ js/main.js
@@ -1,3 +1,5 @@
+let bonusCount = 1;
+
 document.getElementById("add-money-btn").addEventListener("click", function (event) {
     event.preventDefault();
     const addMoneyFrom = document.getElementById("add-money-from").value;
@@ -89,5 +91,140 @@ document.getElementById("withdraw-money-btn").addEventListener("click", function
         }
     } else {
         alert("Not Enough Balance");
+    }
+});
+
+document.getElementById("transfer-money-btn").addEventListener("click", function (event) {
+    event.preventDefault();
+    const transferMoneyTo = document.getElementById("transfer-money-to").value;
+    const transferAmountInput = document.getElementById("transfer-amount");
+    const transferAmount = parseFloat(transferAmountInput.value);
+    const transferPinInput = document.getElementById("transfer-pin");
+    const transferPin = transferPinInput.value;
+
+    const balance = document.getElementById("balance");
+    const convertedBalance = parseFloat(balance.innerText);
+
+    if (transferAmount <= convertedBalance) {
+        if (transferMoneyTo.length === 11) {
+            if (transferPin.length === 4) {
+                const total = convertedBalance - transferAmount;
+                balance.innerText = total;
+
+                const div = document.getElementById("transaction-history");
+                const newDiv = `
+                <div class="flex justify-between items-center py-3 px-4 my-2 bg-white rounded-lg">
+                    <div class="flex justify-between items-center gap-1">
+                        <img class="w-12" src="assets/payoo.png" alt="Payoo">
+                        <div>
+                            <h1 class="font-semibold">Transfer Money</h1>
+                            <p class="text-xs">${transferMoneyTo}</p>
+                        </div>
+                    </div>
+                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6 cursor-pointer">
+                        <path stroke-linecap="round" stroke-linejoin="round" d="M12 6.75a.75.75 0 1 1 0-1.5.75.75 0 0 1 0 1.5ZM12 12.75a.75.75 0 1 1 0-1.5.75.75 0 0 1 0 1.5ZM12 18.75a.75.75 0 1 1 0-1.5.75.75 0 0 1 0 1.5Z" />
+                    </svg>  
+                </div>
+                `
+                div.innerHTML += newDiv;
+
+                transferAmountInput.value = "";
+                transferPinInput.value = "";
+            } else {
+                alert("Invalid Pin Number");
+            }
+        } else {
+            alert("Invalid Bank Account Number");
+        }
+    } else {
+        alert("Not Enough Balance");
+    }
+});
+
+document.getElementById("pay-bill-btn").addEventListener("click", function (event) {
+    event.preventDefault();
+    const payBillTo = document.getElementById("pay-bill-to").value;
+    const payBillAmountInput = document.getElementById("pay-bill-amount");
+    const payBillAmount = parseFloat(payBillAmountInput.value);
+    const payBillPinInput = document.getElementById("pay-bill-pin");
+    const payBillPin = payBillPinInput.value;
+
+    const balance = document.getElementById("balance");
+    const convertedBalance = parseFloat(balance.innerText);
+
+    if (payBillAmount <= convertedBalance) {
+        if (payBillTo.length === 11) {
+            if (payBillPin.length === 4) {
+                const total = convertedBalance - payBillAmount;
+                balance.innerText = total;
+
+                const div = document.getElementById("transaction-history");
+                const newDiv = `
+                <div class="flex justify-between items-center py-3 px-4 my-2 bg-white rounded-lg">
+                    <div class="flex justify-between items-center gap-1">
+                        <img class="w-12" src="assets/payoo.png" alt="Payoo">
+                        <div>
+                            <h1 class="font-semibold">Pay Bill</h1>
+                            <p class="text-xs">${payBillTo}</p>
+                        </div>
+                    </div>
+                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6 cursor-pointer">
+                        <path stroke-linecap="round" stroke-linejoin="round" d="M12 6.75a.75.75 0 1 1 0-1.5.75.75 0 0 1 0 1.5ZM12 12.75a.75.75 0 1 1 0-1.5.75.75 0 0 1 0 1.5ZM12 18.75a.75.75 0 1 1 0-1.5.75.75 0 0 1 0 1.5Z" />
+                    </svg>  
+                </div>
+                `
+                div.innerHTML += newDiv;
+
+                payBillAmountInput.value = "";
+                payBillPinInput.value = "";
+            } else {
+                alert("Invalid Pin Number");
+            }
+        } else {
+            alert("Invalid Bank Account Number");
+        }
+    } else {
+        alert("Not Enough Balance");
+    }
+});
+
+document.getElementById("get-bonus-btn").addEventListener("click", function (event) {
+    event.preventDefault();
+    const bonusCouponInput = document.getElementById("bonus-coupon");
+    const bonusCoupon = bonusCouponInput.value;
+
+    const balance = document.getElementById("balance");
+    const convertedBalance = parseFloat(balance.innerText);
+
+    if (bonusCoupon === "GET100") {
+        if (bonusCount > 0) {
+            const total = convertedBalance + 100;
+            balance.innerText = total;
+            bonusCount--;
+
+            const div = document.getElementById("transaction-history");
+            const newDiv = `
+                <div class="flex justify-between items-center py-3 px-4 my-2 bg-white rounded-lg">
+                    <div class="flex justify-between items-center gap-1">
+                        <img class="w-12" src="assets/payoo.png" alt="Payoo">
+                        <div>
+                            <h1 class="font-semibold">Got Bonus</h1>
+                            <p class="text-xs">GET100</p>
+                        </div>
+                    </div>
+                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6 cursor-pointer">
+                        <path stroke-linecap="round" stroke-linejoin="round" d="M12 6.75a.75.75 0 1 1 0-1.5.75.75 0 0 1 0 1.5ZM12 12.75a.75.75 0 1 1 0-1.5.75.75 0 0 1 0 1.5ZM12 18.75a.75.75 0 1 1 0-1.5.75.75 0 0 1 0 1.5Z" />
+                    </svg>  
+                </div>
+                `
+            div.innerHTML += newDiv;
+
+            bonusCouponInput.value = "";
+        }
+        else {
+            alert("You have already used the bonus.");
+        }
+    } else {
+        alert("Sorry! This Coupon is Expired.");
     }
 });
--- js/tougle.js
+++ js/tougle.js
@@ -1,27 +1,69 @@
 const addMoney = document.getElementById("add-money");
 const cashOut = document.getElementById("cash-out");
 const transaction = document.getElementById("transaction");
+const transferMoney = document.getElementById("transfer-money");
+const payBill = document.getElementById("pay-bill");
+const getBonus = document.getElementById("get-bonus");
 
 addMoney.style.display = "block";
 cashOut.style.display = "none";
 transaction.style.display = "none";
+transferMoney.style.display = "none";
+payBill.style.display = "none";
+getBonus.style.display = "none";
 
 document.getElementById("add-money-card").addEventListener("click", function (event) {
     addMoney.style.display = "block";
     cashOut.style.display = "none";
     transaction.style.display = "none";
+    transferMoney.style.display = "none";
+    payBill.style.display = "none";
+    getBonus.style.display = "none";
 });
 
 document.getElementById("cash-out-card").addEventListener("click", function (event) {
     addMoney.style.display = "none";
     cashOut.style.display = "block";
     transaction.style.display = "none";
+    transferMoney.style.display = "none";
+    payBill.style.display = "none";
+    getBonus.style.display = "none";
 });
 
 document.getElementById("transaction-card").addEventListener("click", function (event) {
     addMoney.style.display = "none";
     cashOut.style.display = "none";
     transaction.style.display = "block";
+    transferMoney.style.display = "none";
+    payBill.style.display = "none";
+    getBonus.style.display = "none";
+});
+
+document.getElementById("transfer-money-card").addEventListener("click", function (event) {
+    addMoney.style.display = "none";
+    cashOut.style.display = "none";
+    transaction.style.display = "none";
+    transferMoney.style.display = "block";
+    payBill.style.display = "none";
+    getBonus.style.display = "none";
+});
+
+document.getElementById("pay-bill-card").addEventListener("click", function (event) {
+    addMoney.style.display = "none";
+    cashOut.style.display = "none";
+    transaction.style.display = "none";
+    transferMoney.style.display = "none";
+    payBill.style.display = "block";
+    getBonus.style.display = "none";
+});
+
+document.getElementById("get-bonus-card").addEventListener("click", function (event) {
+    addMoney.style.display = "none";
+    cashOut.style.display = "none";
+    transaction.style.display = "none";
+    transferMoney.style.display = "none";
+    payBill.style.display = "none";
+    getBonus.style.display = "block";
 });
 
 
