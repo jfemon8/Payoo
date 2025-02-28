a99e575ed9b7de4362326cfb9d3b6c6e72e84c3f
jfemon8 <jfemon8@gmail.com>
2025-02-28 11:38:00 +0600

    Added transaction history section

--- js/main.js
+++ js/main.js
@@ -14,6 +14,23 @@ document.getElementById("add-money-btn").addEventListener("click", function (eve
                 const total = convertedBalance + addAmount;
                 balance.innerText = total;
 
+                const div = document.getElementById("transaction-history");
+                const newDiv = `
+                <div class="flex justify-between items-center py-3 px-4 my-2 bg-white rounded-lg">
+                    <div class="flex justify-between items-center gap-1">
+                        <img class="w-12" src="assets/payoo.png" alt="Payoo">
+                        <div>
+                            <h1 class="font-semibold">Add Money</h1>
+                            <p class="text-xs">${addMoneyFrom}</p>
+                        </div>
+                    </div>
+                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6 cursor-pointer">
+                        <path stroke-linecap="round" stroke-linejoin="round" d="M12 6.75a.75.75 0 1 1 0-1.5.75.75 0 0 1 0 1.5ZM12 12.75a.75.75 0 1 1 0-1.5.75.75 0 0 1 0 1.5ZM12 18.75a.75.75 0 1 1 0-1.5.75.75 0 0 1 0 1.5Z" />
+                    </svg>  
+                </div>
+                `
+                div.innerHTML += newDiv;
+
                 addAmountInput.value = "";
                 addPinInput.value = "";
             } else {
@@ -44,6 +61,23 @@ document.getElementById("withdraw-money-btn").addEventListener("click", function
             if (withdrawPin.length === 4) {
                 const total = convertedBalance - withdrawAmount;
                 balance.innerText = total;
+
+                const div = document.getElementById("transaction-history");
+                const newDiv = `
+                <div class="flex justify-between items-center py-3 px-4 my-2 bg-white rounded-lg">
+                    <div class="flex justify-between items-center gap-1">
+                        <img class="w-12" src="assets/payoo.png" alt="Payoo">
+                        <div>
+                            <h1 class="font-semibold">Withdraw</h1>
+                            <p class="text-xs">${withdrawMoneyTo}</p>
+                        </div>
+                    </div>
+                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6 cursor-pointer">
+                        <path stroke-linecap="round" stroke-linejoin="round" d="M12 6.75a.75.75 0 1 1 0-1.5.75.75 0 0 1 0 1.5ZM12 12.75a.75.75 0 1 1 0-1.5.75.75 0 0 1 0 1.5ZM12 18.75a.75.75 0 1 1 0-1.5.75.75 0 0 1 0 1.5Z" />
+                    </svg>  
+                </div>
+                `
+                div.innerHTML += newDiv;
 
                 withdrawAmountInput.value = "";
                 withdrawPinInput.value = "";
--- js/tougle.js
+++ js/tougle.js
@@ -1,18 +1,29 @@
 const addMoney = document.getElementById("add-money");
 const cashOut = document.getElementById("cash-out");
+const transaction = document.getElementById("transaction");
 
 addMoney.style.display = "block";
 cashOut.style.display = "none";
+transaction.style.display = "none";
 
 document.getElementById("add-money-card").addEventListener("click", function (event) {
     addMoney.style.display = "block";
     cashOut.style.display = "none";
+    transaction.style.display = "none";
 });
 
 document.getElementById("cash-out-card").addEventListener("click", function (event) {
     addMoney.style.display = "none";
     cashOut.style.display = "block";
+    transaction.style.display = "none";
 });
+
+document.getElementById("transaction-card").addEventListener("click", function (event) {
+    addMoney.style.display = "none";
+    cashOut.style.display = "none";
+    transaction.style.display = "block";
+});
+
 
 document.getElementById("logout-btn").addEventListener("click", function (event) {
     window.location.href = "index.html";
